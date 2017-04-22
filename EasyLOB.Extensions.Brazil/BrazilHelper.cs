ee2aec5e55087164c827c39ea122572da06c2ea4
Siegmar Gieseler <siegmar@gmail.com>
2017-04-22 18:30:55 -0300

    -

---
+++ EasyLOB.Extensions.Brazil/BrazilHelper.cs
@@ -0,0 +1,227 @@
+﻿// DocsBr
+// https://github.com/martinusso/docsbr.net
+// https://www.nuget.org/packages/docsbr
+
+// C # - Validando CNPJ , CPF e PIS
+// http://www.macoratti.net/11/09/c_val1.htm
+
+// Classe de Validação de CNPJ,CPF,CEP e Email
+// http://codigofonte.uol.com.br/codigos/classe-de-validacao-de-cnpjcpfcep-e-email
+
+using DocsBr;
+
+namespace EasyLOB.Extensions.Brazil
+{
+    public static class BrazilHelper
+    {
+        #region Methods CNPJ
+
+        public static bool IsCNPJ(string cnpj)
+        {
+            CNPJ cnpjBR = cnpj;
+
+            return cnpjBR.IsValid();
+        }
+
+        public static string CNPJMascara(string cnpj, bool mascara = true)
+        {
+            CNPJ cnpjBR = cnpj;
+
+            if (mascara)
+            {
+                return cnpjBR.ComMascara();
+            }
+            else
+            {
+                return cnpjBR.SemMascara();
+            }
+        }
+
+        #endregion Methods CNPJ
+
+        #region Methods CPF
+
+        public static bool IsCPF(string cpf)
+        {
+            CPF cpfBR = cpf;
+
+            return cpfBR.IsValid();
+        }
+
+        public static string CPFMascara(string cpf, bool mascara = true)
+        {
+            CPF cpfBR = cpf;
+
+            if (mascara)
+            {
+                return cpfBR.ComMascara();
+            }
+            else
+            {
+                return cpfBR.SemMascara();
+            }
+        }
+
+        #endregion Methods CPF
+
+        #region Methods IE
+
+        public static bool IsIE(string ie, int ufIBGE)
+        {
+            IE ieBR = new IE(ie, ufIBGE);
+
+            return ieBR.IsValid();
+        }
+
+        public static bool IsIE(string ie, string uf)
+        {
+            IE ieBR = new IE(ie, uf);
+
+            return ieBR.IsValid();
+        }
+
+        #endregion Methods IE
+
+        #region Methods Telefone
+
+        public static bool IsCodigoPais(string codigoPais)
+        {
+            return true;
+        }
+
+        public static bool IsCodigoArea(string codigoArea)
+        {
+            return true;
+        }
+
+        public static bool IsTelefone(string telefone)
+        {
+            return true;
+        }
+
+        #endregion Methods
+
+        #region Methods
+
+        public static bool IsCEP(string cep)
+        {
+            if (cep.Length == 8)
+            {
+                cep = cep.Substring(0, 5) + "-" + cep.Substring(5, 3);
+            }
+
+            return System.Text.RegularExpressions.Regex.IsMatch(cep, ("[0-9]{5}-[0-9]{3}"));
+        }
+
+        public static bool IsEmail(string email)
+        {
+            return System.Text.RegularExpressions.Regex.IsMatch(email, ("(?<user>[^@]+)@(?<host>.+)"));
+        }
+
+        #endregion Methods
+
+        #region ...
+
+        /*
+        public static bool IsCNPJ(string cnpj)
+        {
+            int[] multiplicador1 = new int[12] { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[13] { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int soma;
+            int resto;
+            string digito;
+            string tempCnpj;
+
+            cnpj = cnpj.Trim();
+            cnpj = cnpj.Replace(".", "").Replace("-", "").Replace("/", "");
+            if (cnpj.Length != 14)
+                return false;
+            tempCnpj = cnpj.Substring(0, 12);
+            soma = 0;
+            for (int i = 0; i < 12; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador1[i];
+            resto = (soma % 11);
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = resto.ToString();
+            tempCnpj = tempCnpj + digito;
+            soma = 0;
+            for (int i = 0; i < 13; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador2[i];
+            resto = (soma % 11);
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = digito + resto.ToString();
+
+            return cnpj.EndsWith(digito);
+        }
+
+        public static bool IsCPF(string cpf)
+        {
+            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            string tempCpf;
+            string digito;
+            int soma;
+            int resto;
+
+            cpf = cpf.Trim();
+            cpf = cpf.Replace(".", "").Replace("-", "");
+            if (cpf.Length != 11)
+                return false;
+            tempCpf = cpf.Substring(0, 9);
+            soma = 0;
+
+            for (int i = 0; i < 9; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = resto.ToString();
+            tempCpf = tempCpf + digito;
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = digito + resto.ToString();
+
+            return cpf.EndsWith(digito);
+        }
+
+        public static bool IsPIS(string pis)
+        {
+            int[] multiplicador = new int[10] { 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int soma;
+            int resto;
+
+            if (pis.Trim().Length != 11)
+                return false;
+            pis = pis.Trim();
+            pis = pis.Replace("-", "").Replace(".", "").PadLeft(11, '0');
+
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+                soma += int.Parse(pis[i].ToString()) * multiplicador[i];
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            return pis.EndsWith(resto.ToString());
+        }
+         */
+
+        #endregion ...
+    }
+}
---
+++ EasyLOB.Extensions.Protheus/ProtheusManager.cs
@@ -0,0 +1,241 @@
+﻿using APConnXControl;
+using EasyLOB.Library;
+using System;
+
+/*
+"Executar como Administrador"
+
+CD \Protheus\API
+COPY apconn.dll C:\Windows\System32
+COPY apconnxcontrol.ocx C:\Windows\System32
+COPY apconn.dll C:\Windows\SysWOW64
+COPY apconnxcontrol.ocx C:\Windows\SysWOW64
+
+CD C:\Windows\SysWOW64
+REGSVR32 apconnxcontrol.ocx
+
+Properties
+    Debugging
+         Enable native code debugging
+ */
+
+namespace EasyLOB.Extensions.Protheus
+{
+    public class ProtheusManager
+    {
+        #region Properties
+
+        protected APConnX Connection = null;
+
+        public string Environment { get; private set; }
+
+        public string Server { get; private set; }
+
+        public int Port { get; private set; }
+
+        public string User { get; private set; }
+
+        public string Password { get; private set; }
+
+        public string Empresa { get; private set; }
+
+        public string Filial { get; private set; }
+
+        public bool IsConnected
+        {
+            get { return Connection.Connected; }
+        }
+
+        public bool IsPrepared { get; private set; }
+
+        public string Error
+        {
+            get { return Connection.LastError; }
+        }
+
+        #endregion Properties
+
+        #region Methods
+
+        public ProtheusManager()
+        {
+            Connection = new APConnX();
+            //Connection.DateFormat = "YYYYMMDD";
+            //Connection.ShowErrors = true;
+
+            Environment = ConfigurationHelper.AppSettings<string>("Protheus.Environment");
+            Server = ConfigurationHelper.AppSettings<string>("Protheus.Server");
+            Port = ConfigurationHelper.AppSettings<int>("Protheus.Port");
+            User = ConfigurationHelper.AppSettings<string>("Protheus.User");
+            Password = ConfigurationHelper.AppSettings<string>("Protheus.Password");
+
+            Empresa = "";
+            Filial = "";
+
+            IsPrepared = false;
+        }
+
+        public void AddLogicalParameter(bool parameter)
+        {
+            Connection.AddLogicalParam(parameter);
+        }
+
+        public void AddDateParameter(DateTime parameter)
+        {
+            Connection.AddDateParamAsDouble(parameter.ToOADate());
+        }
+
+        /*
+        public void AddDateStringParameter(DateTime parameter)
+        {
+            //string dateAsString = String.Format("{0:dd/MM/yyyy}", parameter);
+            //Connection.AddDateParamAsString(dateAsString);
+            //string dateAsString = String.Format("{0:ddMMyyyy}", parameter);
+            //Connection.AddDateParamAsString(dateAsString);
+            string dateAsString = String.Format("{0:yyyyMMdd}", parameter);
+            Connection.AddDateParamAsString(dateAsString);
+        }
+         */
+
+        public void AddNullParameter()
+        {
+            Connection.AddNullParam();
+        }
+
+        public void AddNumericParameter(double parameter)
+        {
+            Connection.AddNumericParam(parameter);
+        }
+
+        public void AddStringParameter(string parameter)
+        {
+            Connection.AddStringParam(parameter);
+        }
+
+        public bool Call(string function)
+        {
+            return Connection.CallProc(function);
+        }
+
+        public void Clear()
+        {
+            Connection.Clear();
+        }
+
+        public bool Connect(string environment = null)
+        {
+            environment = String.IsNullOrEmpty(environment) ? ConfigurationHelper.AppSettings<string>("Protheus.Environment") : environment;
+            if (Environment != environment)
+            {
+                Disconnect();
+            }
+
+            if (!Connection.Connected || Environment != environment)
+            {
+                Connection.Environment = environment;
+                Connection.Server = Server;
+                Connection.Port = Port;
+                Connection.User = User;
+                Connection.Password = Password;
+
+                Environment = environment;
+                Empresa = "";
+                Filial = "";
+
+                Connection.Connect();
+            }
+
+            return Connection.Connected;
+        }
+
+        public bool ConnectAndPrepareEnvironment(string environment = null, string empresa = null, string filial = null)
+        {
+            if (Connect(environment))
+            {
+                PrepareEnvironment(empresa, filial);
+            }
+
+            return IsConnected && IsPrepared;
+        }
+
+        public void Disconnect()
+        {
+            Connection.Disconnect();
+
+            Environment = "";
+            Empresa = "";
+            Filial = "";
+        }
+
+        public bool PrepareEnvironment(string empresa = null, string filial = null)
+        {
+            empresa = String.IsNullOrEmpty(empresa) ? ConfigurationHelper.AppSettings<string>("Protheus.Empresa") : empresa;
+            filial = String.IsNullOrEmpty(filial) ? ConfigurationHelper.AppSettings<string>("Protheus.Filial") : filial;
+            if (!IsPrepared || Empresa != empresa || Filial != filial)
+            {
+                object[] tabelas = { };
+
+                IsPrepared = Connection.PrepareEnv(Environment, empresa, filial, tabelas);
+                if (IsPrepared)
+                {
+                    Empresa = empresa;
+                    Filial = filial;
+                }
+            }
+
+            return IsPrepared;
+        }
+
+        public object ResultToArray()
+        {
+            return Connection.ResultAsArray();
+        }
+
+        public object ResultToDate()
+        {
+            return Connection.ResultAsDate();
+        }
+
+        /*
+        public object ResultToDateString()
+        {
+            return Connection.ResultAsDateString();
+        }
+         */
+
+        public object ResultToLogical()
+        {
+            return Connection.ResultAsLogical();
+        }
+
+        public object ResultToNumeric()
+        {
+            return Connection.ResultAsNumeric();
+        }
+
+        public object ResultToString()
+        {
+            return Connection.ResultAsString();
+        }
+
+        public char GetResultType()
+        {
+            /*
+            65  A   Array
+            66  B   Bloco de Código
+            67  C   Caracter
+            68  D   Data
+            76  L   Lógico
+            77  M   Campo memo
+            78  N   Numérico
+            79  O   Objeto
+            85  U   NIL
+             */
+            short type = Connection.ResultType;
+
+            return Convert.ToChar(type);
+        }
+
+        #endregion Methods
+    }
+}
